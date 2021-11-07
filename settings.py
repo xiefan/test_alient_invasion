66874f3b1a170eb30a0826107d1a058fc56fa8cc
fanxie <xiefan198877@gmail.com>
2021-11-07 17:18:57 -0500

    Initial commit

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    """A class to store settings of the Alien Invasion game"""
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
